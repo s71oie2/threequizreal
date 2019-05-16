71b4a3fbbd2f680889e488aee246e1ed41360717
s71oie2 <s71oie2@gmail.com>
2019-05-16 10:21:39 +0900

    ...Changed...

--- mysite/settings/common.py
+++ mysite/settings/common.py
@@ -96,7 +96,10 @@ DATABASES = {
         'PASSWORD': 'mysql1111',              # DB 비밀번호
         'HOST':     'homework.mysql.pythonanywhere-services.com',               # DB 서버 주소
         'PORT':     '',                        # DB 포트 (생략하면 MySQL 디폴트 포트 3306 자동 지정)
-        'OPTIONS':  {'charset': 'utf8mb4'},
+        'OPTIONS':  {
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        },
     }
 }
 
