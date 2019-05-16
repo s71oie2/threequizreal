2d5da7666d353d483739f57d4007f0f70a3e9cc6
s71oie2 <s71oie2@gmail.com>
2019-05-16 20:22:17 +0900

    ...changed...

--- mysite/settings/common.py
+++ mysite/settings/common.py
@@ -29,8 +29,11 @@ ALLOWED_HOSTS = ['*', ]
 
 
 # Application definition
-
 INSTALLED_APPS = [
+    'staff',
+    'quiz',
+    'plant',
+    'user',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,11 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # 'django.contrib.sites',
     # 'django.contrib.humanize',
-    'staff',
-    'quiz',
-    'plant',
-    'user',
+    
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -79,29 +80,38 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+    'default': {
+            'ENGINE':   'django.db.backends.mysql',
+            'NAME':     'threewheel',                  # DB 이름
+            'USER':     'root',                    # DB 사용자 이름
+            'PASSWORD': 'mysql1111',              # DB 비밀번호
+            'HOST':     '127.0.0.1',               # DB 서버 주소
+            'PORT':     '',                        # DB 포트 (생략하면 MySQL 디폴트 포트 3306 자동 지정)
+            'OPTIONS':  {'charset': 'utf8mb4'},
+        }
+}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'ENGINE':   'django.db.backends.mysql',
+#         'NAME': 'homework$threewheel',                  # DB 이름
+#         # 'TEST': {
+#         #             'NAME': 'homework$test_threewheel',
+#         #         },
+#         'USER':     'homework',                    # DB 사용자 이름
+#         'PASSWORD': 'mysql1111',              # DB 비밀번호
+#         'HOST':     'homework.mysql.pythonanywhere-services.com',               # DB 서버 주소
+#         'PORT':     '',                        # DB 포트 (생략하면 MySQL 디폴트 포트 3306 자동 지정)
+#         'OPTIONS':  {
+#             'charset': 'utf8mb4',
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+#         },
 #     }
 # }
-DATABASES = {
-    'default': {
-        'ENGINE':   'django.db.backends.mysql',
-        'NAME': 'homework$threewheel',                  # DB 이름
-        # 'TEST': {
-        #             'NAME': 'homework$test_threewheel',
-        #         },
-        'USER':     'homework',                    # DB 사용자 이름
-        'PASSWORD': 'mysql1111',              # DB 비밀번호
-        'HOST':     'homework.mysql.pythonanywhere-services.com',               # DB 서버 주소
-        'PORT':     '',                        # DB 포트 (생략하면 MySQL 디폴트 포트 3306 자동 지정)
-        'OPTIONS':  {
-            'charset': 'utf8mb4',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        },
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
